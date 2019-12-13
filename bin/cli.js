fc6d70707daffaba7559f2c0fb3dbd36db57beff
unknown <2844735407>
2019-12-13 14:19:27 +0800

    A 简单的一些配置

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
 console.log('hello world');
+
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-n, --name [name]', 'name')
+  .option('-e, --email [email]', 'email')
+  .parse(process.argv)
+
+if(program.name) {
+  console.log(`Hello, ${program.name}! (${program.email})  What's your`)
+}
